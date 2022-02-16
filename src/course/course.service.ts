3bb23551c91db520e7523f850052def00ac601f4
“[Marlon <[marlonmolina10@hotmail.com]>
2022-02-16 11:00:18 +0000

    Error handling

--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Course } from './entities/course.entity';
 
 @Injectable()
@@ -12,26 +12,35 @@ export class CourseService {
         }
     ];
 
-    findAll(){
+    findAll() {
         return this.courses;
     }
 
-    findOne(id: string){
-        return this.courses.find(course => course.id === Number(id));
+    findOne(id: string) {
+        const course = this.courses.find(course => course.id === Number(id));
+
+        if (!course) {
+            throw new HttpException(
+                `Course Id ${id} not found`,
+                HttpStatus.NOT_FOUND)
+            ;
+        }
+
+        return course
     }
 
-    create(createCourseDto: any){
+    create(createCourseDto: any) {
         this.courses.push(createCourseDto);
     }
 
-    update(id: string, updateCourseDto: any){
+    update(id: string, updateCourseDto: any) {
         const indexCourse = this.courses.findIndex(course => course.id === Number(id));
         this.courses[indexCourse] = updateCourseDto;
     }
 
-    remove(id: string){
+    remove(id: string) {
         const indexCourse = this.courses.findIndex(course => course.id === Number(id));
-        if(indexCourse >= 0){
+        if (indexCourse >= 0) {
             this.courses.splice(indexCourse, 1);
         }
 
